547291626928949c423a42d15f8a458c80b33c20
ErnieWhite <ernie.white.68@gmail.com>
2024-11-20 06:32:36 -0600

    Added command line argument handling

--- addprefix.c
+++ addprefix.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+	if (argc == 1){ // use the current directory as the prefix
+		printf("Use the directory name\n");
+	} else if (argc == 2){ // use the value passed in from the command line as the prefix
+		printf("Use the command line argument\n");
+	} else {
+		printf("Usage: addprefix <prefix>\n");
+		printf("If prefix is omitted, the directory name is used\n");
+		return 1;
+	}
+	
 	return 0;
 }
